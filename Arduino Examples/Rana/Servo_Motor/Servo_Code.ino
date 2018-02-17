6969e596ad6b0a72bb88ae6896842886a7907fbe
RanaReda <33959845+Rana2@users.noreply.github.com>
2018-02-18 00:11:27 +0200

    Update Servo_Code.ino

--- Arduino Examples/Rana/Servo_Motor/Servo_Code.ino
+++ Arduino Examples/Rana/Servo_Motor/Servo_Code.ino
@@ -1,30 +1,3 @@
-/*
-SparkFun Inventor's Kit
-Example sketch 08-1
-
-SINGLE SERVO
-
-  Sweep a servo back and forth through its full range of motion.
-
-  A "servo", short for servomotor, is a motor that includes 
-  feedback circuitry that allows it to be commanded to move to
-  specific positions. This one is very small, but larger servos
-  are used extensively in robotics to control mechanical arms,
-  hands, etc. You could use it to make a (tiny) robot arm,
-  aircraft control surface, or anywhere something needs to be
-  moved to specific positions.
-
-
-This sketch was written by SparkFun Electronics,
-with lots of help from the Arduino community.
-This code is completely free for any use.
-Visit http://learn.sparkfun.com/products/2 for SIK information.
-Visit http://www.arduino.cc to learn about the Arduino.
-
-Version 2.0 6/2012 MDG
-*/
-
-
 #include <Servo.h>  // servo library
 
 
