3c2875f0da58f17d0f974faef1e7e5d56cf95eb8
RanaReda <33959845+Rana2@users.noreply.github.com>
2018-02-20 17:36:29 +0200

    Sheet1 Solution Q2

---
+++ Arduino Examples/Rana/Q2/Q2_Code.ino
@@ -0,0 +1,21 @@
+void setup(){
+}
+void loop(){
+
+for ( int i=4 ;i<=7 ; i++){
+
+digitalWrite(i, HIGH);
+delay(1000);
+digitalWrite(i, LOW);
+delay(1000);
+
+}
+for(int i=7; i>5 ; i--){
+
+digitalWrite(i-1, HIGH);
+delay(1000);
+digitalWrite(i-1, LOW);
+delay(1000);
+
+}
+}
