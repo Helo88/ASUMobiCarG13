a392d6b4906ba6abaf0b93a1a9c4c8257805f41a
shimaa-mahmoud <shamiaa220@gmail.com>
2018-04-09 01:48:15 +0200

    Add files via upload

---
+++ Arduino Examples/Shimaa/ir test.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (6,INPUT);
+;pinMode (7,INPUT)
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int ls = digitalRead(6);
+int ls = digitalRead(7);.
+if ((ls==LOW) && (rs == LOW))
+//FORWARD
+if ((ls==LOW) && (rs == HIGH))
+//URN RIGHR
+
+if ((ls==HIGH) && (rs == LOW))
+// TURN LEFT
+if ((ls==HIGH) && (rs == HIGH))
+//STOP
+}
